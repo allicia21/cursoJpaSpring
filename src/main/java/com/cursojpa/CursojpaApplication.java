048d78d8b8add9e7f5d2403cf67da6a7b79ba349
Allicia Sousa <allicia.ass33@gmail.com>
2023-04-19 20:07:19 -0300

    Criando operacao de instanciacao

--- src/main/java/com/cursojpa/CursojpaApplication.java
+++ src/main/java/com/cursojpa/CursojpaApplication.java
@@ -1,13 +1,36 @@
 package com.cursojpa;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursojpaApplication {
+import com.cursojpa.domain.Categoria;
+import com.cursojpa.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursojpaApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursojpaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+		
+		
 	}
 
 }
